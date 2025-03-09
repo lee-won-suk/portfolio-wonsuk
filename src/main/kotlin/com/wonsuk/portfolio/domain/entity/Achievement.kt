b3613054f855eb519a16f64236cb19099f8de23e
tjdjdjrdl123 <lee-won-suk@github.io>
2025-03-09 19:35:24 +0900

    feat: 데이터베이스 초기화

--- src/main/kotlin/com/wonsuk/portfolio/domain/entity/Achievement.kt
+++ src/main/kotlin/com/wonsuk/portfolio/domain/entity/Achievement.kt
@@ -1,13 +1,14 @@
 package com.wonsuk.portfolio.domain.entity
 
 import jakarta.persistence.*
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 @Entity
 class Achievement(
     title: String,
     description: String,
-    achievedDate: LocalDateTime?,
+    achievedDate: LocalDate,
     host: String,
     isActive: Boolean
 ) : BaseEnitity() {
@@ -20,7 +21,7 @@ class Achievement(
 
     var description: String = description
 
-    var achievedDate: LocalDateTime? = achievedDate
+    var achievedDate: LocalDate = achievedDate
 
     var host: String = host
 
