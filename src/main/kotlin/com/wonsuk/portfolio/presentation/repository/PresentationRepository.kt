f2f0ee620469a90eb672dc9b714ac26d0ebb6339
tjdjdjrdl123 <lee-won-suk@github.io>
2025-03-13 12:28:14 +0900

    feat: 리포지토리 개발

--- src/main/kotlin/com/wonsuk/portfolio/presentation/repository/PresentationRepository.kt
+++ src/main/kotlin/com/wonsuk/portfolio/presentation/repository/PresentationRepository.kt
@@ -1,7 +1,41 @@
 package com.wonsuk.portfolio.presentation.repository
 
+import com.wonsuk.portfolio.domain.entity.*
+import com.wonsuk.portfolio.domain.repository.*
 import org.springframework.stereotype.Repository
 
 @Repository
-class PresentationRepository {
+class PresentationRepository(
+    private val achievementRepository: AchievementRepository,
+    private val introductionRepository: IntroductionRepository,
+    private val skillRepository: SkillRepository,
+    private val linkRepository: LinkRepository,
+    private val projectRepository: ProjectRepository,
+    private val experienceRepository: ExperienceRepository
+) {
+    fun getActiveAchievements(): List<Achievement> {
+        return achievementRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveExperiences(): List<Experience> {
+        return experienceRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveIntroductions(): List<Introduction> {
+        return introductionRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveSkills(): List<Skill> {
+        return skillRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveLinks(): List<Link> {
+        return linkRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveProjects(): List<Project> {
+        return projectRepository.findAllByIsActive(true)
+    }
+
+
 }
